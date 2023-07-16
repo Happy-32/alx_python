7eb647aed81a664adc10d57401ea717e347cd221
Happy-32 <yahayaenmaariandrew@gmain.com>
2023-07-16 19:37:47 +0000

    python-Hello World

---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = str(number)[-1]
+def is_greater(last_digit):
+    if int(last_digit) > 5:
+        return "and is greater than 5"
+    elif int(last_digit) == 0:
+        return "and is zero"
+    elif int(last_digit) < 6 and int(last_digit) != 0:
+        return "and is less than 6 and not 0"
+
+res = is_greater(last_digit)
+print("Last digit of {} is {} {}".format(number,last_digit, res))
