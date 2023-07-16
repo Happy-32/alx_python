873b09853548147e9e40a580de8bc74c2afcfce3
Happy-32 <yahayaenmaariandrew@gmain.com>
2023-07-16 01:42:20 +0000

    python-Hello World

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -5,4 +5,4 @@ last_2 = word[-2:]
 middle = word[1:-1]
 print("First 3 letters: {}".format(first_3))
 print("Last 2 letters: {}".format(last_2))
-print("Middle word: {}".format(middle))
+print("Middle word: {}\n".format(middle))
