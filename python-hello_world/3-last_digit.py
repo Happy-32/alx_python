01b0e3b96860322962dab5a0bd3a2d4e13bcbe9b
Happy-32 <yahayaenmaariandrew@gmain.com>
2023-07-23 15:28:07 +0000

    Python Hello_World

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,16 +2,23 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = str(number)[-1]
-def is_greater(last_digit):
-    if int(last_digit) > 5:
-        return "and is greater than 5"
-    elif int(last_digit) == 0:
-        return "and is zero"
-    elif int(last_digit) < 6 and int(last_digit) != 0:
-        return "and is less than 6 and not 0"
+# def is_greater(last_digit):
+#     if int(last_digit) > 5:
+#         return "and is greater than 5"
+#     elif int(last_digit) == 0:
+#         return "and is zero"
+#     elif int(last_digit) < 6 and int(last_digit) != 0:
+#         return "and is less than 6 and not 0"
 
-res = is_greater(last_digit)
-if number > 0:
-    print("Last digit of {} is {} {}".format(number,last_digit, res))
+# res = is_greater(last_digit)
+# if number > 0:
+#     print("Last digit of {} is {} {}".format(number,last_digit, res))
+# elif number < 0:
+#     print("Last digit of {} is -{} {}".format(number,last_digit, res))
+
+if last_digit == 0:
+    print("last digit of ",number, "is ",last_digit)
+elif number > 0:
+    print("Last digit of ",number, "is ",last_digit)
 elif number < 0:
-    print("Last digit of {} is -{} {}".format(number,last_digit, res))
+    print("Last digit of ",number, "is -{}".format(last_digit))
