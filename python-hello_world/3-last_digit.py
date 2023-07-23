7e624bf317a656ba3c9de820319e55c763cf942f
Happy-32 <yahayaenmaariandrew@gmain.com>
2023-07-23 15:58:39 +0000

    Python Hello_World

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -10,17 +10,17 @@ if number > 0:
     if last_digit > 5:
         print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
     elif last_digit == 0:
-        print("Last digit of {} is 0".format(number))
+        print("Last digit of {} is 0 and is 0".format(number))
     elif last_digit < 6 and last_digit != 0:
         print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
 
 elif number < 0:
     last_digit = last_num(number)
     if last_digit == 0:
-        print("Last digit of {} is 0".format(number))
+        print("Last digit of {} is 0 and is 0".format(number))
     else:
         print("Last digit of {} is -{} and is less than 6 and not 0".format(number, last_digit))
 
 else:
-    print("Last digit of {} is 0".format(number))
+    print("Last digit of {} is 0 and is 0".format(number))
 
