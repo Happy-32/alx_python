bdbae7fd934ef612af262321e94a410d4385acd0
Happy-32 <yahayaenmaariandrew@gmain.com>
2023-07-29 17:18:08 +0000

    importing modules

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,5 +1,4 @@
-# check_arguments.py
-
+#!/usr/bin/python3
 import sys
 
 def args():
@@ -7,6 +6,10 @@ def args():
 
     if len(sys.argv) == 1:
         print("{} arguments.".format(len(sys.argv)-1))
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(len(sys.argv)-1))
+        for i in range(1,len(sys.argv),1):
+            print("{}: {}".format(i,sys.argv[i]))
     else:
         print("{} arguments:".format(len(sys.argv)-1))
         for i in range(1,len(sys.argv),1):
