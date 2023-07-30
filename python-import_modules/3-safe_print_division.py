52509fce2444f4aa6a809a7abf49d67ef1515ee9
Happy-32 <yahayaenmaariandrew@gmain.com>
2023-07-30 10:57:28 +0000

    importing modules

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        dividend = a / b
+        if b != 0:
+            dividend = a / b
     except Exception as e:
         return None
     finally:
-        if b != 0:
-            print("Inside Result: {}".format(dividend))
+        print("Inside Result: {}".format(dividend))
+
 
 # print(safe_print_division(10,5))
 # print(safe_print_division(12,2))
