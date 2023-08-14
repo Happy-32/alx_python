98246c727eeae7530c37f55fcf488d53d2a0acb4
Happy-32 <yahayaenmaariandrew@gmain.com>
2023-08-14 23:48:31 +0000

    classes and objects

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+
+    def size_isInt(self):
+        try:
+            if isinstance(self.__size, int):
+                return self.__size
+        except TypeError:
+            print('size must be an integer')
+
+    def size_lzero(self):
+        try:
+            if self.__size > 0:
+                return self.__size
+        except ValueError:
+            print("size must be >= 0")
