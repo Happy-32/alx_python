1b17d3d04d36108f11fee61ba0fc98cf2d37f534
Happy-32 <yahayaenmaariandrew@gmain.com>
2023-08-18 23:32:34 +0000

    classes and objects

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+"""
+Square model defines a square based on its size
+
+-Square: a class that represents the square
+"""
 class Square:
+    """
+    Attributes:
+    -size(int): the size of the square which should be an integer
+
+    Methods:
+    -__init__(size=0): Initializes the square object 
+    """
     def __init__(self, size):
         self.__size = size
--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""
+Square model defines a square based on its size
+
+-Square: a class that represents the square
+"""
 class Square:
+    """
+    Attributes:
+    -size(int): the size of the square which should be an integer
+
+    Methods:
+    -__init__(size=0): Initializes the square object 
+    """
     def __init__(self, size=0):
         self.__size = size
 
