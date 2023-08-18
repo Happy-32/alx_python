2082ad67903126b919e0a5ef9d488c004e18405d
Happy-32 <yahayaenmaariandrew@gmain.com>
2023-08-18 23:39:39 +0000

    classes and objects

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -12,29 +12,13 @@ class Square:
     Methods:
     -__init__(size=0): Initializes the square object 
     """
-    # def __init__(self, size=0):
-    #     if not isinstance(size, int):
-    #         raise TypeError("size must be an integer")
-    #     elif size < 0:
-    #         raise ValueError("size must be >= 0")
-    #     else:
-    #         self.__size = size
-
     def __init__(self, size=0):
-        self.__size = size
-
-    def size_isInt(self):
-        try:
-            if not isinstance(self.__size, int):
-                return self.__size
-        except TypeError:
-            print('size must be an integer')
-
-    def size_lzero(self):
-        try:
-            if self.__size < 0:
-                print("size must be an >= 0")
-        except ValueError:
-            print("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    
 
 
