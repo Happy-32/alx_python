9fe55f0e8d0796a55ce8d59f004f6ef7b9e0de36
Happy-32 <yahayaenmaariandrew@gmain.com>
2023-08-19 00:24:45 +0000

    classes and objects

---
+++ python-classes/4-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Square model defines a square based on its size
+
+-Square: a class that represents the square
+"""
+class Square:
+    """
+    Attributes:
+    -size(int): the size of the square which should be an integer
+
+    Methods:
+    -__init__(size=0): Initializes the square object 
+
+    -size(): to access the size attribute of the Square object
+    -area(): to compute the area of the square
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self,value):
+        self.__size = value
+
+    def area(self):
+        if isinstance(self.size, int):
+            return self.size * self.size
+        else:
+            raise TypeError("size must be an integer")
+
+    def my_print(self):
+        if self.size == 0:
+            print("",end="")
+        else:
+            for i in range(self.size):
+                print("#" * self.size)
