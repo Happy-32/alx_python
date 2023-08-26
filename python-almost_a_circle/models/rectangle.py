cd8071b59949c709c218a1828b4d8e9f2394467c
Happy-32 <yahayaenmaariandrew@gmain.com>
2023-08-26 22:40:52 +0000

    almost a circle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python3
-from base import Base
+"""
+Base defines a python class 
+
+-Base: class representing Base
+"""
+class Base:
+    """
+    __init__(self, id=None)- Attribute to checK if id is None
+
+    __init__(id = None): initializes the Base object
+    __nb_objects: a class level attribute
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1 #Base allows you to access all class level attributes
+            self.id = Base.__nb_objects
+
+
 class Rectangle(Base):
     """
     Rectangle represents a Rectangle object that inherits from the Base class
@@ -111,4 +132,3 @@ class Rectangle(Base):
         """
         self.__y = y
 
-#
