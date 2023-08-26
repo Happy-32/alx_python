bc534dd96366311230fded45d12cc6960bebda4f
Happy-32 <yahayaenmaariandrew@gmain.com>
2023-08-26 01:27:08 +0000

    almost a circle

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -8,13 +8,13 @@ class Base:
     """
     __init__(self, id=None)- Attribute to chect if id is None
 
-    __init__(id = NOne): initializes the Base object
+    __init__(id = None): initializes the Base object
     """
     __nb_objects = 0
 
     def __init__(self, id=None):
         if id is not None:
             self.id = id
-        else:
+        elif id == None:
             Base.__nb_objects += 1 #Base allows you to access all class level attributes
             Base.id = self.__nb_objects
