e880e61abeba77b0a94a39e85672a3a926cdbd64
Happy-32 <yahayaenmaariandrew@gmain.com>
2023-08-27 17:09:45 +0000

    almost a circle

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,8 +1,11 @@
 from rectangle import Rectangle
 
 class Square(Rectangle):
+    """
+    Defines a square class that inherits from rectangle class
+    """
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(x, y, size, size, id)
+        Rectangle.__init__(x, y, size, size, id)
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
