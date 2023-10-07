df5418ad399dafb6308fa9f3704cad7d3bb78360
Andrew Yahaya <yahayaenmaariandrew@gmail.com>
2023-10-07 16:00:11 +0000

    Python ORMs

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    # Check if all three arguments are provided
+    if len(sys.argv) != 4:
+        print("Please provide 3 arguments: MySQL username, MySQL password, and database name")
+        sys.exit(1)
+
+    # Get the arguments from the command line
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # print("{}, {}, {}".format(username,password,database))
+
+    def select_state(username, password, database):
+        database = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database)
+        cursor = database.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+
+        cursor.close()
+        database.close()
+
+    
+    select_state(username, password, database)
---
+++ python-object_relational_mapping/2-my_filter_States.py
@@ -0,0 +1,31 @@
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    # Check if all three arguments are provided
+    if len(sys.argv) != 5:
+        print("Please provide 3 arguments: MySQL username, MySQL password, and database name")
+        sys.exit(1)
+
+    # Get the arguments from the command line
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+
+
+    def select_state(username, password, database, state):
+        # print("{}, {}, {}, {}".format(username,password,database, state))
+        
+        database = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database)
+        cursor = database.cursor()
+        cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (state,))
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+
+        cursor.close()
+        database.close()
+
+
+    select_state(username, password, database, state)
