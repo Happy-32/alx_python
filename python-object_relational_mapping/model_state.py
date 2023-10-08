e42bfcb2950958591a9d7971b0bed4ebf6994d36
Andrew Yahaya <yahayaenmaariandrew@gmail.com>
2023-10-08 15:51:42 +0000

    Python ORMs

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,11 +1,28 @@
-#!/usr/bin/python3
-"""Start link class to table in database 
-"""
+from sqlalchemy import create_engine, Column, Integer, String
 import sys
-from model_state import Base, State
-
-from sqlalchemy import (create_engine)
+from sqlalchemy.orm import declarative_base
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    if len(sys.argv) != 4:
+        print("Please provide 3 arguments: MySQL username, MySQL password, database name")
+        sys.exit(1)
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+path = "mysql+mysqldb://{}:{}@localhost/{}".format(username, password, database)
+
+database = create_engine(path)
+Connection = database.connect()
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+Base.metadata.create_all(bind=database)
+# State("Arizona")
