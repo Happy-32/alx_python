29758a3564155abf830ca48dab71789c218d22d2
Andrew Yahaya <yahayaenmaariandrew@gmail.com>
2023-10-21 12:17:28 +0000

    Python Web Framework

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
+        row = ""
         for j in i:
-            print("{:d}".format(j),end=" ")
-        print()
+            row += str(j) + " "
+        print(row[:-1])
