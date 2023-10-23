744cc1e6977dc0f5f1f4845664b27a313eb0c49e
Andrew Yahaya <yahayaenmaariandrew@gmail.com>
2023-10-23 01:30:44 +0000

    Functions

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 def fibonacci_sequence(n):
     fib_seq = [0, 1]
-    for i in range(2, n):#n not inclusive
-        next_digit = fib_seq[i-1] + fib_seq[i-2]
-        fib_seq.append(next_digit)
+    if n == 0:
+        fib_seq = []
+    elif n == 1:
+        fib_seq = [0]
+    else:
+        for i in range(2, n):#n not inclusive
+            next_digit = fib_seq[i-1] + fib_seq[i-2]
+            fib_seq.append(next_digit)
     return fib_seq
