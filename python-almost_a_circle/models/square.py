dd52ea709b68905c83639807bf6c1488ec19dc04
Andrew Yahaya <yahayaenmaariandrew@gmail.com>
2023-10-27 00:19:50 +0000

    python almost a circle

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,13 +4,27 @@ Square defines a python class that inherits from Rectangle
 from .rectangle import Rectangle
 class Square(Rectangle):
     """
-    Defines a square class that inherits from rectangle class
+    ....
     """
     def __init__(self, size, x=0, y=0, id=None):
         if size <= 0:
             raise ValueError("size must be > 0")
-        super().__init__(x, y, size, size, id)
+        super().__init__(size, size, x, y, id)
+    
+    def area(self):
+        """
+        Computes the area of the square
+        """
+        return self.width * self.width
 
+    def display(self):
+        """
+        Displays the square as a series of '#' characters
+        """
+        for _ in range(self.y):
+            print()
+        for _ in range(self.height):
+            print(" " * self.x + "#" * self.width)
+    
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
-    
