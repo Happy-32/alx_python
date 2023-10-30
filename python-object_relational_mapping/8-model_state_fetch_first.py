8321380101be1296e96216a76d18a2e0f935376e
Andrew Yahaya <yahayaenmaariandrew@gmail.com>
2023-10-30 01:01:00 +0000

    python ORM

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,5 @@
 import sys
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select
 from model_state import Base, State
 if len(sys.argv) != 4:
     print("Insufficient number of arguments, please provide 'username', 'password' and 'database name' to procede")
@@ -16,7 +16,7 @@ Base.metadata.bind = database
 # Base.metadata.create_all(bind=database)
 connection = database.connect()
 
-state = connection.execute((State.__table__.select().order_by(State.id)).fetchone())
+state = connection.execute(select(State).order_by(State.id)).fetchone()
 
 if state:
     print("{}: {}".format(state.id, state.name))
