7d685219ae22f1265e6f1de77475f3554d24284d
Andrew Yahaya <yahayaenmaariandrew@gmail.com>
2023-10-31 12:01:06 +0000

    Network

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,8 +1,19 @@
 import requests
 import sys
 
-# print(sys.argv[1])
-request = requests.get(sys.argv[1])
-# print(dir(request))
-print(request.headers["x-request-id"])
+if len(sys.argv) < 2:
+    print("Usage: python script.py <URL>")
+    sys.exit(1)
 
+url = sys.argv[1]
+
+try:
+    response = requests.get(url)
+
+    x_request_id = response.headers.get("x-request-id")
+
+    if x_request_id:
+        print(x_request_id)
+    
+except requests.exceptions.RequestException as e:
+    print(f"Error making the request: {e}")
