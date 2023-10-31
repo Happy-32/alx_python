95125d5d635385afdc65824b31cc7ab701c85291
Andrew Yahaya <yahayaenmaariandrew@gmail.com>
2023-10-31 15:16:37 +0000

    Network

---
+++ python-network_1/5-json_apy.py
@@ -0,0 +1,33 @@
+import requests
+import sys
+
+if len(sys.argv) < 2:
+    print("insufficient Args")
+
+letter = sys.argv[1]
+
+def search(letter):   
+    url = "http://0.0.0.0:5000/search_user"
+
+    data = {"q": letter}
+    try:
+        request = requests.post(url, data=data)
+
+        if request.status_code == 200:
+            try:
+                info = request.json()
+                if info:
+                    user_id = info.get('id')
+                    user_name = info.get('name')
+                    print("[{}] {}".format(user_id, user_name))
+                else:
+                    print("No result")
+            except ValueError:
+                print("Not a valid JSON")
+        else:
+            print("No result")
+
+    except requests.exceptions.RequestException as e:
+        print("Request could not be made")
+
+search(letter)
