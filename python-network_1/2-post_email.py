35f83458e89517900d142bd4db5d59a88767398f
Andrew Yahaya <yahayaenmaariandrew@gmail.com>
2023-10-31 12:20:57 +0000

    Network

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -10,6 +10,15 @@ email = sys.argv[2]
 
 try:
     request = requests.post(url, {'email': email})
+    # print("Email: ",request,email)
+
+    if request.status_code == 200:
+        print("success")
+    else:
+        print('Failed')
+    
+    print("{}".format(email))
     # pass
 except requests.exceptions.RequestException as e:
+    # print(e)
     pass
