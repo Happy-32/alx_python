4c7b0c0fde7250a08678a1879e61b96cddb5fa4c
Andrew Yahaya <yahayaenmaariandrew@gmail.com>
2023-11-01 11:55:50 +0000

    Network

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -2,32 +2,43 @@ import requests
 import sys
 
 
-if len(sys.argv) < 2:
-    print("No result")
+# if len(sys.argv) < 2:
+#     print("No result")
 letter = sys.argv[1]
 
 def search(letter):   
     url = "http://0.0.0.0:5000/search_user"
 
     data = {"q": letter}
+    request = requests.post(url, data=data)
     try:
-        request = requests.post(url, data=data)
-
-        if request.status_code == 200:
-            try:
-                info = request.json()
-                if info:
-                    user_id = info.get('id')
-                    user_name = info.get('name')
-                    print("[{}] {}".format(user_id, user_name))
-                else:
-                    print("No result")
-            except ValueError:
-                print("Not a valid JSON")
+        json = request.json()
+        if json:
+            print("[{}] {}".format(json.get('id'), json.get('name')))
         else:
             print("No result")
+    except ValueError:
+        print("Not a valid JSON")
 
-    except requests.exceptions.RequestException as e:
-        print("Request could not be made")
+
+    # try:
+    #     request = requests.post(url, data=data)
+
+    #     if request.status_code == 200:
+    #         try:
+    #             info = request.json()
+    #             if info:
+    #                 user_id = info.get('id')
+    #                 user_name = info.get('name')
+    #                 print("[{}] {}".format(user_id, user_name))
+    #             else:
+    #                 print("No result")
+    #         except ValueError:
+    #             print("Not a valid JSON")
+    #     else:
+    #         print("No result")
+
+    # except requests.exceptions.RequestException as e:
+    #     print("Request could not be made")
 
 search(letter)
