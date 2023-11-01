55d5faaf6df8bd7e811f64a9833c666d05ed2572
Andrew Yahaya <yahayaenmaariandrew@gmail.com>
2023-11-01 12:00:59 +0000

    Network

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -2,7 +2,7 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    letter = sys.argv[1]
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
 
 def search(letter):   
     url = "http://0.0.0.0:5000/search_user"
