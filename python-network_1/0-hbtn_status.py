6d12e20763de80d9d5a2ee4fe02c6013394d9174
Andrew Yahaya <yahayaenmaariandrew@gmail.com>
2023-11-01 10:56:20 +0000

    Network

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,4 +3,7 @@ import requests
 request = requests.get("https://alu-intranet.hbtn.io/status")
 # print(dir(request))
 # statusCode = request.status_code
-print("Body response:\n  - type:", type(request.text), "\n  - content:", request.text,end="")
+# print("Body response:\n  - type:", type(request.text), "\n  - content:", request.text)
+body_response = request.text[:53]
+
+print("Body response:\n  - type:", type(body_response), "\n  - content:", body_response)
