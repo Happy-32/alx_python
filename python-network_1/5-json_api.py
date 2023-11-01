1a44e3da4c1fdbd4339a4b0e13a151b51a08a86f
Andrew Yahaya <yahayaenmaariandrew@gmail.com>
2023-11-01 11:59:24 +0000

    Network

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -9,34 +9,34 @@ def search(letter):
 
     data = {"q": letter}
     request = requests.post(url, data=data)
-    try:
-        json = request.json()
-        if json:
-            print("[{}] {}".format(json.get('id'), json.get('name')))
-        else:
-            print("No result")
-    except ValueError:
-        print("Not a valid JSON")
-
-
     # try:
-    #     request = requests.post(url, data=data)
-
-    #     if request.status_code == 200:
-    #         try:
-    #             info = request.json()
-    #             if info:
-    #                 user_id = info.get('id')
-    #                 user_name = info.get('name')
-    #                 print("[{}] {}".format(user_id, user_name))
-    #             else:
-    #                 print("No result")
-    #         except ValueError:
-    #             print("Not a valid JSON")
+    #     json = request.json()
+    #     if json:
+    #         print("[{}] {}".format(json.get('id'), json.get('name')))
     #     else:
     #         print("No result")
+    # except ValueError:
+    #     print("Not a valid JSON")
 
-    # except requests.exceptions.RequestException as e:
-    #     print("Request could not be made")
+
+    try:
+        request = requests.post(url, data=data)
+
+        if request.status_code == 200:
+            try:
+                info = request.json()
+                if info:
+                    user_id = info.get('id')
+                    user_name = info.get('name')
+                    print("[{}] {}".format(user_id, user_name))
+                else:
+                    print("No result")
+            except ValueError:
+                print("Not a valid JSON")
+        else:
+            print("No result")
+
+    except requests.exceptions.RequestException as e:
+        print("Request could not be made")
 
 search(letter)
