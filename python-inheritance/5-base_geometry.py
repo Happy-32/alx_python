ce9165ec821511e71dbd2afa97622adf592f8540
Andrew Yahaya <yahayaenmaariandrew@gmail.com>
2023-11-02 14:57:18 +0000

    Inheritance

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -2,13 +2,22 @@
 .....
 """
 
-class BaseGeometry:
-    """
-    .....
-    """
+class meta_class(type):
+    def __dir__(cls):
+        return [attr for attr in super().__dir__() if attr != "__init_subclass__"] 
 
-    def __dir__(self):
-        return [attr for attr in dir(type(self)) if attr != '__init_subclass__']
+
+class BaseGeometry(metaclass=meta_class):
+    """
+    An Empty class
+    """
+    def __dir__(cls) -> None:
+        return_array = super().__dir__()
+        return_dir = []
+        for i in return_array:
+            if i != "__init_subclass__":
+                return_dir.append(i)
+        return return_dir
 
     def area(self):
         raise Exception("area() is not implemented")
