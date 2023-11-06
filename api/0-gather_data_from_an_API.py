a264e1d89e62d28147f8bfd87b47a2bbc3bf762f
Andrew Yahaya <yahayaenmaariandrew@gmail.com>
2023-11-06 15:45:59 +0000

    API

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+import requests
+import json
+import sys
+
+id = sys.argv[1]
+
+todoItemsUrl = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(id)
+employeeUrl = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+
+todoResponse = requests.get(todoItemsUrl)
+employeeResponse = requests.get(employeeUrl)
+todo = todoResponse.json()
+employee = employeeResponse.json()
+
+employeeName = employee['name']
+totalTasks = len(todo)
+taskCompleted = sum(1 for i in todo if i['completed'])
+
+print('Emplyee {} is done with tasks ({}/{})'.format(employeeName, taskCompleted, totalTasks))
+for i in todo:
+    if i['completed']:
+        print('\t',i['title'])
+
---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,37 @@
+import requests
+import json
+import sys
+import csv
+
+id = sys.argv[1]
+
+def getTodo(id):
+    todoItemsUrl = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(id)
+    employeeUrl = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+
+    todoResponse = requests.get(todoItemsUrl)
+    employeeResponse = requests.get(employeeUrl)
+    todo = todoResponse.json()
+    employee = employeeResponse.json()
+    employeeName = employee['username']
+    totalTasks = len(todo)
+    taskCompleted = sum(1 for i in todo if i['completed'])
+
+    csvFile = []
+    csvFile.append(['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
+    for i in todo:
+        csvFile.append([id,employeeName, str(i['completed']), i['title']])
+    
+
+    with open('USER_ID.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(csvFile)
+
+
+    # print('Emplyee {} is done with tasks ({}/{})'.format(employeeName, taskCompleted, totalTasks))
+    # for i in todo:
+    #     if i['completed']:
+    #         print('\t',i['title'])
+
+getTodo(id)
+
