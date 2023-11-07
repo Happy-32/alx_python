d898d94eb3d5423a9ca479d369cebc647f0fb6b2
Andrew Yahaya <yahayaenmaariandrew@gmail.com>
2023-11-07 09:25:25 +0000

    api

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,58 +1,52 @@
 """
-This module exports data in the JSON format.
+This module exports the response to a json file
 """
 
 import json
 import requests
 import sys
 
-def get_employee_info(employee_id):
-    # Define the API endpoints
-    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    todos_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
-
-    # Fetch employee information
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
-
-    if 'id' not in user_data:
-        print(f"USER_ID {employee_id} is not valid.")
-        return
-
-    employee_id = user_data.get('id')
-    employee_name = user_data.get('username')
-
-    # Fetch employee's TODO list
-    todos_response = requests.get(todos_url)
-    todos_data = todos_response.json()
-
-    # Create a JSON data structure
-    employee_json_data = {
-        "USER_ID": [
-            {
-                "task": todo['title'],
-                "completed": todo['completed'],
-                "username": employee_name
-            }
-            for todo in todos_data
-        ]
-    }
-
-    # Write JSON data to a file
-    json_filename = f'{employee_id}.json'
-    with open(json_filename, 'w') as json_file:
-        json.dump(employee_json_data, json_file, indent=4)
-
-    print(f"Data has been exported to {json_filename}.")
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
+    if len(sys.argv) !=2:
+        print("Enter an id")
         sys.exit(1)
 
     try:
-        employee_id = int(sys.argv[1])
-        get_employee_info(employee_id)
+        employeeId = int(sys.argv[1])
     except ValueError:
-        print("Employee ID must be an integer.")
+        print("emplpyee id must be an integer")
         sys.exit(1)
+
+
+def employeeID(employeeId):
+
+    todoItemsUrl = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(id)
+    employeeUrl = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+
+    todoResponse = requests.get(todoItemsUrl)
+    employeeResponse = requests.get(employeeUrl)
+    todo = todoResponse.json()
+    employee = employeeResponse.json()
+
+    employeeId = employee.get('id')
+    employeeName = employee.get('username')
+
+
+    employee_json_data = {
+    "USER_ID": [
+        {
+            "task": task['title'],
+            "completed": task['completed'],
+            "username": employeeName
+        }
+        for task in todo
+    ]
+}
+
+    # Write JSON data to a file
+    json_filename = f'{employeeId}.json'
+    with open(json_filename, 'w') as json_file:
+        json.dump(employee_json_data, json_file, indent=4)
+
+employeeID(id)
+
